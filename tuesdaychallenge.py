deb5147e6f7d9184a7ce689c6c1e17d4b1097583
Ahir-Hitesh <ahir.hitesh81@gmail.com>
2023-01-08 21:01:43 +0000

    First commit to learn about version controlling

---
+++ tuesdaychallenge.py
@@ -0,0 +1,22 @@
+#str(gatsby["quantity_in_stock"])
+#!/usr/env/python3
+
+
+wordbank= ["indentation", "spaces"] 
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric',
+              'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 
+              'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
+wordbank.append(4)
+
+num = input("Please enter a number between 0 and 20\n")
+
+newNum = int(num)
+
+
+student_name = tlgstudents.slice[newNum]
+
+# This is what you should see when print runs-
+# Hi <name>! Welcome to Day 2 of Python Training!
+print("Hi " + name.capitalize() + "! Welcome to Day " + str(mylist[1]) + " of " + mylist[5]  + " Training!")
+
