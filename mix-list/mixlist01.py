849e3eafbe4a1f534261ed057d726618da1f136f
Ahir-Hitesh <ahir.hitesh81@gmail.com>
2023-01-10 22:01:35 +0000

    mixed lists(redoing some labs to review

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-# display only the IP addresses to the screen.
-iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
-# example 1 - add up the strings
-print("IP addresses: " + iplist[3] + ", and " + iplist[4])
+#ipList containing six items
+iplist = [5060, "80", 55, "10.0.0.1","10.20.30.1", "ssh"]
 
-# example 2 - use the comma separator
-print("IP addresses:", iplist[3], ", and", iplist[4])
+#printing only ip addresses in iplist
+print("IP Addresses : ", iplist[3], ", and ", iplist[4])
 
-# example 3 - use an 'f-string'
-print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
+#print item two in list
+#print ("The second item in the list (port): " + str( my_List[1]))
+
+#print item three in list
+#print ("The last item in the list (state): " + my_List[2])
+
 
--- tuesdaychallenge.py
+++ tuesdaychallenge.py
@@ -1,22 +1,10 @@
-#str(gatsby["quantity_in_stock"])
 #!/usr/env/python3
 
+mylist= [1,2,3,4,5, "Python"]
 
-wordbank= ["indentation", "spaces"] 
-tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric',
-              'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 
-              'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
-
-wordbank.append(4)
-
-num = input("Please enter a number between 0 and 20\n")
-
-newNum = int(num)
-
-
-student_name = tlgstudents.slice[newNum]
+name= input("What is your name?\n")
 
 # This is what you should see when print runs-
 # Hi <name>! Welcome to Day 2 of Python Training!
-print("Hi " + name.capitalize() + "! Welcome to Day " + str(mylist[1]) + " of " + mylist[5]  + " Training!")
+print("Hi ",  name.capitalize() , "! Welcome to Day " , mylist[1] , " of " , mylist[5], " Training!")
 
