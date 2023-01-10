4acb97d5cdc09c8a019fceda0423b19e819ff21f
Ahir-Hitesh <ahir.hitesh81@gmail.com>
2023-01-10 05:54:26 +0000

    join and split

---
+++ ask_input.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def main():
+
+    user_name = input("Please enter your name: ")
+    
+    Day = input("Please enter day of the week today is: ")
+    
+    print("Hello, " + user_name," Happy " + Day, sep="!")
+main()
