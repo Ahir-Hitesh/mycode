d0fa1db9e1107245504e91840cf215a9b1aad3a8
Ahir-Hitesh <ahir.hitesh81@gmail.com>
2023-01-11 06:54:04 +0000

    Some more List ops

---
+++ advlist/animal-list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" List - making selections from complex lists"""
+
+
+def main():
+    #Create a list of at least five animals with three letter names 
+    myList = ["Fox", "Fly", "Ant", "Bee", "Cat"]
+
+    # display animal in the list based on position provided
+    print(myList[4], myList[2], myList[0], myList[3])
+main()
