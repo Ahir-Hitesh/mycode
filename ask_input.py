be06f2948177aa7c6726b63b1a2e9810dd86e91c
Ahir-Hitesh <ahir.hitesh81@gmail.com>
2023-01-12 06:32:01 +0000

    more changes in file structure

--- ask_input.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-def main():
-
-    user_name = input("Please enter your name: ")
-    
-    Day = input("Please enter day of the week today is: ")
-    
-    print("Hello, " + user_name," Happy " + Day, sep="!")
-main()
